2954bcd558f1db35ac1ae9cc787a71aae3108d6e
kiko0o <kirilalamurov@gmail.com>
2022-12-09 13:11:43 +0200

    playRound logic

--- main.js
+++ main.js
@@ -1,7 +1,70 @@
 //Rock Paper Scissors Game
 
-//Step 1: Function which randomly returns a string
+let getComputerChoice = () => {
+    let choices = Array("Rock", "Paper", "Scissors");
+    let randomIndex = [Math.floor(Math.random()*choices.length)];
+    let randomItem = choices[randomIndex];
+    return randomItem;
+}
 
-let choices = Array("Rock", "Paper", "Scissors");
-let getComputerChoice = () => [Math.floor(Math.random() * choices.length)];
-console.log(getComputerChoice());
+let playRound = (playerSelection, ComputerSelection) => {
+
+playerSelectionFixed = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+switch (ComputerSelection) {
+    case "Rock":
+        switch (playerSelectionFixed) {
+            case "Scissors":
+                console.log("Player: Scissors, Computer: Rock");
+                return "Lose!";
+                break;
+            case "Paper":
+                console.log("Player: Paper, Computer: Rock");
+                return "Win!";
+                break;
+            case "Rock":
+                console.log("Player: Rock, Computer: Rock");
+                return "Even!";
+                break;
+        }
+        break;
+
+    case "Paper":
+        switch (playerSelectionFixed) {
+            case "Scissors":
+                console.log("Player: Scissors, Computer: Paper");
+                return "Win!";
+                break;
+            case "Paper":
+                console.log("Player: Paper, Computer: Paper");
+                return "Even!";
+                break;
+            case "Rock":
+                console.log("Player: Rock, Computer: Paper");
+                return "Lose!";
+                break;
+        }
+        break;
+
+    case "Scissors":
+        switch (playerSelectionFixed) {
+            case "Scissors":
+                console.log("Player: Scissors, Computer: Scissors");
+                return "Even!";
+                break;
+            case "Paper":
+                console.log("Player: Paper, Computer: Scissors");
+                return "Lose!";
+                break;
+            case "Rock":
+                console.log("Player: Rock, Computer: Scissors");
+                return "Win!";
+                break;
+        }
+        break;
+}
+
+}
+
+const playerSelection = "rOcK";
+const ComputerSelection = getComputerChoice();
+console.log(playRound(playerSelection,ComputerSelection));
